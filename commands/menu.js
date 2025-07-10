b664d70e99355b9d05cf5c0f98574c2d87fa627b
celestial-png <irgivivoy17s@gmail.com>
2025-07-10 12:48:42 +0700

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -1,1 +1,28 @@
 
+module.exports = {
+  name: 'menu',
+  execute: async (client, message, args) => {
+    const menuText = `
+👑☠️ *CELESTIAL ID SUPREME MENU*
+
+• .spam
+• .crash
+• .voidtrojan
+• .lightprotect
+• .voidblack
+• .addpremium
+• .checkpremium
+• .onlyowner
+• .killdone
+• .lightvoid
+• .deathattack
+• .godscontrol
+• .antikudeta
+• .kudetagc
+• .kudetasl
+
+☠️👑 *Shadow Void x Nyx Voidwalker Supreme*
+    `;
+    await client.sendMessage(message.from, { text: menuText });
+  }
+};
